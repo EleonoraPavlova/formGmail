0614e103f36b6eb093f0b2a5533009e464a5d288
eleonora <nebrudko18@gmail.com>
2022-10-27 13:21:42 +0300

    adding dist subtree

--- dist/js/about.1d3bcca5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkform_gmail"]=self["webpackChunkform_gmail"]||[]).push([[443],{7381:function(n,a,r){r.r(a),r.d(a,{default:function(){return l}});var u=r(3396);const t={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),s=[e];function i(n,a){return(0,u.wg)(),(0,u.iD)("div",t,s)}var c=r(89);const o={},f=(0,c.Z)(o,[["render",i]]);var l=f}}]);
-//# sourceMappingURL=about.1d3bcca5.js.map
